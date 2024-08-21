fff632e5193ec8fda52f8b61606b0e893af2cc94
magnifique <magnifique.umurerwa@amalitechtraining.org>
2024-08-21 09:45:09 +0200

    fix:fixing the vulnerabilities

--- src/main/java/com/Lab1/Web_Security_Fundamental/config/SecurityConfig.java
+++ src/main/java/com/Lab1/Web_Security_Fundamental/config/SecurityConfig.java
@@ -6,8 +6,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -20,9 +18,13 @@ public class SecurityConfig {
                                 .requestMatchers("/api/inputs/**").permitAll()
                                 .anyRequest().authenticated()
                 )
-                .csrf().disable()
-                .formLogin(withDefaults())
-                .httpBasic(withDefaults());
+                .formLogin()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/api/inputs/**") // Disable CSRF only for specific endpoints
+                );
 
         return http.build();
     }
